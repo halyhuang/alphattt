b0422d9628bdc617a818e7fcf3bb62df36aa1ee5
haly <284662@qq.com>
2016-11-19 22:38:05 +0800

    fix guest bug

--- src/deploy.erl
+++ src/deploy.erl
@@ -11,6 +11,7 @@ run() ->
 	db_api:add_user("mcts_ucb1", "", robot),
 	db_api:add_user("mcts_ucb1_pall", "", robot),	
 	db_api:add_user("mcts_value", "", robot),	
+	db_api:add_user("mcts_always", "", robot),		
 	db_api:add_user("kk", "123456", human),	
 	db_api:add_user("KK", "123456", human),	
 	db_api:add_user("tt", "123456", human),	
