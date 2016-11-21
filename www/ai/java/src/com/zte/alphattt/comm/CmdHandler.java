1c8f80c0a3e8ae41941f24a795e098dff420194a
haly <284662@qq.com>
2016-11-21 23:31:52 +0800

    rm sourcefiles

--- www/ai/java/src/com/zte/alphattt/comm/CmdHandler.java
+++
@@ -1,9 +0,0 @@
-package com.zte.alphattt.comm;
-
-import com.ericsson.otp.erlang.OtpErlangObject;
-
-public interface CmdHandler {
-	boolean isMe(OtpErlangObject otpErlangObject);
-
-	void handle(PlayerClient adapter, OtpErlangObject otpErlangObject);
-}
--- www/ai/java/src/com/zte/alphattt/game/Player.java
+++
@@ -1,8 +0,0 @@
-package com.zte.alphattt.game;
-
-public interface Player {
-	void update(int[] move, int[] gameState);
-	int[] getMove();
-	void display(int[] move, int[] gameState);
-	void setThinkMiliSecs(int miliSecs);
-}
